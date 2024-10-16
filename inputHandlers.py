4dd62a6e35604b33d44538f8baf3351ea581ff22
Will Schweitzer <weschweitzer@gmail.com>
2024-10-16 12:11:56 -0400

    Add InventoryActivate/DropEventHandlers

--- inputHandlers.py
+++ inputHandlers.py
@@ -5,6 +5,7 @@ from typing import Optional, TYPE_CHECKING
 from tcod import libtcodpy
 import tcod.event
 
+import actions
 from actions import ( 
     Action,
     BumpAction,
@@ -193,6 +194,26 @@ class InventoryEventHandler(AskUserEventHandler):
     def on_item_selected(self, item: Item) -> Optional[Action]:
         """Called when the user selects a valid item."""
         raise NotImplementedError()
+
+
+class InventoryActivateHandler(InventoryEventHandler):
+    """Handle using an inventory item."""
+
+    TITLE = "Select an item to use"
+
+    def onItemSelected(self, item: Item) -> Optional[Action]:
+        """Return the action for the selected item."""
+        return item.consumable.getAction(self.engine.player)
+
+
+class InventoryDropHandler(InventoryEventHandler):
+    """Handle dropping an inventory item."""
+
+    TITLE = "Select an item to drop"
+
+    def onItemSelected(self, item: Item) -> Optional[Action]:
+        """Drop this item."""
+        return actions.DropItem(self.engine.player, item)
 
 
 class MainGameEventHandler(EventHandler):
