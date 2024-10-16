8e37dd10ecd470ab999f325ef06128f3cf79f328
Will Schweitzer <weschweitzer@gmail.com>
2024-10-16 11:50:36 -0400

    Add inventory component)

---
+++ components/inventory.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+from typing import List, TYPE_CHECKING
+
+from components.baseComponent import BaseComponent
+
+if TYPE_CHECKING:
+    from entity import Actor, Item
+
+
+class Inventory(BaseComponent):
+    parent: Actor
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.items: List[Item] = []
+
+    def drop(self, item: Item) -> None:
+        """
+        Removes an item from the inventory and restores it to the game map, at the player's current location.
+        """
+        self.items.remove(item)
+        item.place(self.parent.x, self.parent.y, self.gameMap)
+
+        self.engine.messageLog.addMessage(f"You dropped the {item.name}.")
