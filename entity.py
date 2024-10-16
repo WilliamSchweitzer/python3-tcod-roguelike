d76d8e564e5870ff80349085bb7407a9854011cc
Will Schweitzer <weschweitzer@gmail.com>
2024-10-16 11:36:03 -0400

    Add Item subclass to Enitity

--- entity.py
+++ entity.py
@@ -7,6 +7,7 @@ from renderOrder import RenderOrder
 
 if TYPE_CHECKING:
     from components.ai import BaseAI
+    from components.consumable import Consumable
     from components.fighter import Fighter
     from gameMap import GameMap
 
@@ -99,3 +100,27 @@ class Actor(Entity):
     def isAlive(self) -> bool:
         """Returns True as long as this actor can perform actions."""
         return bool(self.ai)
+
+class Item(Entity):
+    def __init__(
+        self,
+        *,
+        x: int = 0,
+        y: int = 0,
+        char: str = "?",
+        color: Tuple[int, int, int] = (255, 255, 255),
+        name: str = "<Unamed>",
+        consumable: Consumable,
+    ):
+        super().__init__(
+            x=x,
+            y=y,
+            char=char,
+            color=color,
+            name=name,
+            blocksMovement=False,
+            renderOrder=RenderOrder.ITEM,
+        )
+
+        self.consumable = consumable
+        self.consumable.parent = self
