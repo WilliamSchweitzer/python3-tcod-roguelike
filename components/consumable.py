978da3c00b638ae6fda8601fe056d70794e2c793
Will Schweitzer <weschweitzer@gmail.com>
2024-11-15 11:25:07 -0500

    Update camelCase

--- components/consumable.py
+++ components/consumable.py
@@ -52,28 +52,28 @@ class HealingConsumable(Consumable):
             raise Impossible(f"Your health is already full.")
 
 class LightningDamageConsumable(Consumable):
-    def __init__(self, damage: int, maximum_range: int):
+    def __init__(self, damage: int, maximumRange: int):
         self.damage = damage
-        self.maximum_range = maximum_range
+        self.maximumRange = maximumRange
 
     def activate(self, action: actions.ItemAction) -> None:
         consumer = action.entity
         target = None
-        closest_distance = self.maximum_range + 1.0
+        closestDistance = self.maximumRange + 1.0
 
         for actor in self.engine.game_map.actors:
             if actor is not consumer and self.parent.gamemap.visible[actor.x, actor.y]:
                 distance = consumer.distance(actor.x, actor.y)
 
-                if distance < closest_distance:
+                if distance < closestDistance:
                     target = actor
-                    closest_distance = distance
+                    closestDistance = distance
 
         if target:
-            self.engine.message_log.add_message(
+            self.engine.messageLog.addMessage(
                 f"A lightning bolt strikes the {target.name} with a loud thunder, for {self.damage} damage!"
             )
-            target.fighter.take_damage(self.damage)
+            target.fighter.takeDamage(self.damage)
             self.consume()
         else:
             raise Impossible("No enemy is close enough to strike.")
